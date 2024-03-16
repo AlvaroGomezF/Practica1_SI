04ed1b67d6c6587100b114c0fda5bcc147d57675
AlvaroGomezF <126874322+AlvaroGomezF@users.noreply.github.com>
2024-03-16 12:38:35 +0100

    Consultas acabado

--- Consultas.py
+++ Consultas.py
@@ -10,7 +10,7 @@ conn = sqlite3.connect('BBDD.db')
 query="SELECT COUNT(*) AS num_muestras FROM users"
 df = pd.read_sql_query(query, conn)
 num_muestras = df['num_muestras'].iloc[0]  # Obtener el valor de 'num_muestras' de la primera fila
-print("Ejericicio 1")
+print("Apartado 1")
 print("Número total de muestras:", num_muestras)
 
 #Ejercicio 2:
@@ -19,7 +19,7 @@ df = pd.read_sql_query(query, conn)
 total_fecha=df['fecha'].nunique()
 media_fechas = total_fecha / len(df)  # La media es el total de fechas únicas dividido por el total de registros
 desviacion_fechas = np.sqrt(((df['ip'].value_counts() - media_fechas) ** 2).sum() / len(df))  # La desviación estándar
-print("Ejercicio 2")
+print("Apartado 2")
 print("Media de IPs detectadas:", media_fechas)
 print("Desviación estándar de IPs detectadas:", desviacion_fechas)
 
@@ -27,22 +27,53 @@ print("Desviación estándar de IPs detectadas:", desviacion_fechas)
 total_ips = df['ip'].nunique()
 media_ips = total_ips / len(df)  # La media es el total de IPs únicas dividido por el total de registros
 desviacion_ips = np.sqrt(((df['ip'].value_counts() - media_ips) ** 2).sum() / len(df))  # La desviación estándar
-print("Ejercicio 3")
+print("Apartado 3")
 print("Media de fechas:", media_ips)
 print("Desviación estándar de fechas :", desviacion_ips)
 
 #Ejercicio 4:
-query = """
+print("Apartado 4")
+query1 = """
     SELECT SUM(cliclados_emails) AS total_phishing_emails
     FROM users 
 """
-df = pd.read_sql_query(query, conn)
+df = pd.read_sql_query(query1, conn)
 sumaTotal = df['total_phishing_emails'].iloc[0]
 print("Media de clicados : "+str(sumaTotal/num_muestras))
 
-'''
-desviacion_estandar = df['total_phishing_emails'].std()
+query2=query = """
+    SELECT cliclados_emails
+    FROM users 
+"""
+df2 = pd.read_sql_query(query2, conn)
+desviacion=df2['cliclados_emails'].std()
+print("Desviacion tipica clicados : "+str(desviacion))
 
-print("Media de correos electrónicos de phishing recibidos por usuario:", sumaTotal)
-print("Desviación estándar de correos electrónicos de phishing recibidos por usuario:", desviacion_estandar)'''
+#Ejercicio 5:
+print("Apartado 5")
+query = """
+    SELECT MIN(total_emails) AS min_total_emails, MAX(total_emails) AS max_total_emails
+    FROM users
+"""
 
+df=pd.read_sql_query(query,conn)
+min_total_emails = df['min_total_emails'].iloc[0]
+max_total_emails = df['max_total_emails'].iloc[0]
+
+print("Valor mínimo del total de emails recibidos:", min_total_emails)
+print("Valor máximo del total de emails recibidos:", max_total_emails)
+
+
+#Ejercicio 6:
+print("Apartado 6")
+query = """
+    SELECT MIN(cliclados_emails) AS min_emails, MAX(cliclados_emails) AS max_emails
+    FROM users WHERE permisos=1
+"""
+
+df=pd.read_sql_query(query,conn)
+min_total_emails = df['min_emails'].iloc[0]
+max_total_emails = df['max_emails'].iloc[0]
+
+print("Numero mínimo  emails interactuados admin:", min_total_emails)
+print("Numero máximo  emails interactuados admin:", max_total_emails)
