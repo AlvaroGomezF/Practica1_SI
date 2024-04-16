bd3967400ad35fcdaf71441f9603e8b448095454
inajerus <a.inarejos2021@gmail.com>
2024-04-16 18:18:35 +0200

    terminado ejercicio 4 practica 2

--- Ejercicio4Practica2.py
+++ Ejercicio4Practica2.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import numpy as np
 import sqlite3
+import hashlib
+
+
 
 conn = sqlite3.connect('BBDD.db')
+cursor = conn.cursor()
 
 # Obtener datos de conexiones de usuarios por día
 query = """
@@ -22,3 +26,44 @@ conexiones_por_dia = df.groupby(['dia', 'ip']).size().reset_index(name='conexion
 
 print("Conexiones por día de usuario:")
 print(conexiones_por_dia)
+
+
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS usuarioslogin (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE,
+        password TEXT
+    )
+''')
+conn.commit()
+
+def registrar_usuario(username, password):
+    # Hash de la contraseña
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    try:
+        cursor.execute('''
+            INSERT INTO usuarioslogin (username, password) VALUES (?, ?)
+        ''', (username, hashed_password))
+        conn.commit()
+        print("Usuario registrado exitosamente.")
+    except sqlite3.IntegrityError:
+        print("El nombre de usuario ya está en uso.")
+
+def verificar_credenciales(username, password):
+    # Hash de la contraseña proporcionada
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    cursor.execute('''
+        SELECT * FROM usuarioslogin WHERE username=? AND password=?
+    ''', (username, hashed_password))
+    user = cursor.fetchone()
+    if user:
+        print("Inicio de sesión exitoso.")
+    else:
+        print("Credenciales incorrectas.")
+
+# Ejemplo de registro de usuario
+registrar_usuario('usuario2', 'password1234')
+
+# Ejemplo de inicio de sesión
+verificar_credenciales('usuario2', 'password1234')
