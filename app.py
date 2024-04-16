ea75b5f441e305d9110c2f344c1089f619962eba
aGuzman01 <a.guzman.2021@alumnos.urjc.es>
2024-04-16 17:42:27 +0200

    ejer3

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import Ejercicio2
 import Ejercicio3
 import Ejercicio4
 import matplotlib
-app = Flask(__name__)
+app = Flask(_name_)
 
 
 @app.route('/')
@@ -13,6 +13,16 @@ def index():
 
 @app.route("/ejercicio2")
 def ejercicio2():
-    df_results=Ejercicio2.obtenerDataQuery()
+    df_results=ejercicio2.obtenerDataQuery()
 
 
+@app.route("/ejercicio3")  # Ruta para el ejercicio 3
+def ejercicio3():
+    # Obtener estadísticas desde Ejercicio3
+    stats_correos, stats_contrasenas = Ejercicio3.estadisticas()
+    cont_campos_vacios_usuarios, cont_campos_vacios_admin = Ejercicio3.obtener_usuarios_campos_vacios()
+
+    # Renderizar la plantilla HTML y pasar los datos
+    return render_template('ejercicio3.html', stats_correos=stats_correos, stats_contrasenas=stats_contrasenas,
+                           cont_campos_vacios_usuarios=cont_campos_vacios_usuarios,
+                           cont_campos_vacios_admin=cont_campos_vacios_admin)
